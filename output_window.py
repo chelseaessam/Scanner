6a72214c7a55783f9270a3b5e57e4575d5cb9179
chelseaessam <36311453+chelseaessam@users.noreply.github.com>
2019-10-28 11:32:25 +0200

    syntanx error and comments are handled

--- output_window.py
+++ output_window.py
@@ -21,19 +21,39 @@ class OutputWindow(QMainWindow,FORM_CLASS):
         self.buttons()
  def buttons(self):
      self.pushButton.clicked.connect(self.goback)
+     
+ def show_msgBox(self,msg):
+        self.msgBox=QMessageBox()
+        self.msgBox.setWindowTitle("Error!")
+        self.msgBox.setIcon(QMessageBox.Warning)
+        self.msgBox.setText(msg)
+        self.msgBox.setStandardButtons(QMessageBox.Ok)
+        self.msgBox.exec_()
+        
  def goback(self):
      self.close()
      self.main_window=MainWindow()
      self.main_window.show()
  def scanner(self,text):
+      Is_bracket=False
       for word in text:
          Is_number=False
          Is_identifier=False
-         if word in reserved:
+         
+
+         if (Is_bracket==True) and( word!= "}" or word[len(word)-1]!="}"):
+             continue
+         elif Is_bracket==True and word=="}":
+            Is_bracket=False
+            continue
+         
+         elif word in reserved:
                  self.mylist.addItem(word+",reserved")
                  
+                 
          elif word in symbols:
-                 self.mylist.addItem(word+",symbols")
+                 self.mylist.addItem(word+",special symbols")
+                 
                  
          elif word[0].isdigit():
              for letter in word:
@@ -44,13 +64,26 @@ class OutputWindow(QMainWindow,FORM_CLASS):
                      break
              if Is_number==True:
               self.mylist.addItem(word+",number")
+             else:
+                self.show_msgBox("Syntax Error found "+word)
+                break
+
+              
          elif word[0].isalpha():
              for letter in word:
                  if letter.isalpha():
                      Is_identifier=True
                  else:
                      Is_identifier=False
-                     self.mylist.addItem(word+", error")
                      break
              if Is_identifier==True:
                  self.mylist.addItem(word+" , identifier")
+             else:
+                self.show_msgBox("Syntax Error found in "+word)
+                break
+            
+         elif word =="{" or word[0]=="{":
+                Is_bracket=True
+                
+
+             
